e199863fc847bb25a029bb388ac13d23292428f0
Troy Dohner <troy@husointeractive.com>
2014-03-27 01:02:26 -0700

    adding shortcut script

---
+++ lites.sh
@@ -0,0 +1,11 @@
+python CM19aDriver.py D 2 OFF
+espeak -s 100 -a 180 -p 85 "OFF"
+sleep 1
+python CM19aDriver.py D 2 ON
+espeak -s 100 -a 180 -p 85 "ON"
+sleep 1
+python CM19aDriver.py D 2 OFF
+espeak -s 100 -a 180 -p 85 "OFF"
+sleep 1
+python CM19aDriver.py D 2 ON
+espeak -s 100 -a 180 -p 85 "ON"
